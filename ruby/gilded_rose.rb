e681dd58f6c3db9d0571c33e2728e203d19c20c6
Ajit <ajit.scs@gmail.com>
2023-12-12 12:56:21 +0530

    Changes in update quantity logic with dynamic product based methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -2,55 +2,108 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @method_mapping = {
+      '+5 Dexterity Vest' => 'dexterity',
+      'Aged Brie' => 'aged_brie',
+      'Elixir of the Mongoose' => 'elixir',
+      'Sulfuras, Hand of Ragnaros' => 'sulfuras',
+      'Backstage passes to a TAFKAL80ETC concert' => 'backstage',
+      'Conjured Mana Cake' => 'conjured'
+    }.freeze
   end
+  attr_accessor :method_mapping
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    @items.map { send("process_#{method_mapping[_1.name]}", _1) && update_sell_in(_1) }
   end
+
+  def process_dexterity(item)
+    item.quality -= 1
+  end
+
+  def process_backstage(item)
+    return if item.quality >= 50
+    return item.quality += 2 if item.sell_in > 5 && item.sell_in <= 10
+    return item.quality += 3 if item.sell_in <= 5
+    
+    # item.quality = 0
+  end
+
+  def update_sell_in(item)
+    item.sell_in -= 1
+  end
+
+  def process_aged_brie(item)
+    return if item.quality >= 50
+    return item.quality += 2 if item.sell_in <= 0
+    item.quality += 1
+  end
+
+  def process_elixir(item)
+    process_common(item)
+  end
+
+  def process_sulfuras(item)
+    return
+  end
+
+  def process_conjured(item)
+    process_common(item)
+  end
+
+  def process_common(item)
+    return item.quality if item.quality.zero?
+    return item.quality = 0 if item.sell_in <= 0
+    item.quality -= 1
+  end
+
+  # def update_quality()
+  #   @items.each do |item|
+  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #       if item.quality > 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros"
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     if item.name != "Sulfuras, Hand of Ragnaros"
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality = item.quality + 1
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
 
 class Item
